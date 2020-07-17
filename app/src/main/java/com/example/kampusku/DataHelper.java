a34aa38395b9d96d4c943583bb8f98028beb9a35
nurilmuslichin16 <nurilmuslichin16@gmail.com>
2020-07-18 06:50:00 +0700

    Upload aplikasi Kampusku

---
+++ app/src/main/java/com/example/kampusku/DataHelper.java
@@ -0,0 +1,31 @@
+package com.example.kampusku;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "kampusku.db";
+    private static final int DATABASE_VERSION = 1;
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table mahasiswa(nomor integer primary key, nama text null, tgl text null, jk text null, alamat text null);";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+        sql = "INSERT INTO mahasiswa(nomor, nama, tgl, jk, alamat) VALUES ('1', 'Nuril Muslichin', '1999-02-16', 'Laki-Laki', 'Kota Pekalongan');";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        
+    }
+
+}
